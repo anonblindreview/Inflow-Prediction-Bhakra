ebf2406e28eaed339c3cd2c535b1ade07e2f4b18
Anurag <tellanurag95@gmail.com>
2019-05-03 16:06:31 +0530

    one year tfm

--- thomas feiring model/daily_thomas_feiring.py
+++ thomas feiring model/daily_thomas_feiring.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 #enter the year for which you need prediction starting 2019
 year=2019
-number_of_days=366*2
+number_of_days=365
 day=0
 df = pd.read_csv('../LSTM/groundtruth.csv')
 u=df['Mean']
@@ -18,9 +18,10 @@ for i in range(number_of_days):
     rn=np.random.normal(0,1,1)[0]
     z_t=(X_t-u[day])/sd[day]
     z_t1=lag[day]*z_t+rn*math.sqrt(1-lag[day]*lag[day])
-    X_t1=u[(day+1)%12]+z_t1*sd[(day+1)%12]
-    print(day,",",year,",",X_t1)
-    if(day==365):
+    X_t1=u[(day+1)%365]+z_t1*sd[(day+1)%365]
+    print(day+1,",",year,",",X_t1)
+    if(day==364):
         year=year+1
-    day=(day+1)%366
+        day=0
+    day=day+1
     X_t=X_t1
