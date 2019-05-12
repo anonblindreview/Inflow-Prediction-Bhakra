fc05a4d2200a295fcaad218abcd1804e324754b2
Anurag <tellanurag95@gmail.com>
2019-05-12 13:01:55 +0530

    final

--- LSTM/inference/latest_trend.py
+++ LSTM/inference/latest_trend.py
@@ -1,40 +1,53 @@
-from keras.utils.vis_utils import model_to_dot
 import numpy as np
 from keras.models import load_model
-from keras.utils.vis_utils import plot_model
 from pandas import read_csv
 from sklearn.preprocessing import MinMaxScaler
-# fix random seed for reproducibility
+def create_dataset(dataset, look_back=1):
+    dataX, dataY = [], []
+    for i in range(len(dataset)-look_back-1):
+        a = dataset[i:(i+look_back)]
+        dataX.append(a)
+        dataY.append(dataset[i + look_back])
+    return np.array(dataX), np.array(dataY)
+
+######### FIX RANDOM SEED FOR REPRODUCIBILITY ##
 np.random.seed(7)
-# load the dataset
-dataframe = read_csv('data1.csv', usecols=[2], engine='python', skipfooter=3)
+
+######### LOAD THE DATASET #####################
+dataframe = read_csv('../data1.csv', usecols=[2], engine='python', skipfooter=3)
+
+dataframe1 = read_csv('../data observed.csv', usecols=[5],engine='python', skipfooter=3)
+dataframe.append(dataframe1,ignore_index = True)
 dataset = dataframe.values
 dataset = dataset.astype('float32')
-# normalize the dataset
+
+######### NORMALIZE THE DATASET ################
 scaler = MinMaxScaler(feature_range=(0, 1))
 dataset = scaler.fit_transform(dataset)
+
+######### LOAD THE MODEL #######################
+
 path=input("what is type of model you wish to load?")
-model=load_model("weights/"+path+".h5")
+model=load_model("../weights/"+path+".h5")
 print(model.summary())
-print("plotting model")
-plot_model(model, to_file='model_plot/'+path+'.png', show_shapes=True, show_layer_names=True)
-#SVG(model_to_dot(model).create(prog='dot', format='svg'))
-print("model plotted")
 batch_size=int(input("Enter batch size"))
 look_back=3
-data=[]
-for items in dataset[-look_back:]:
-    data.append(items[0])
-ndataset=[]
-for i in range(batch_size):
-    ndataset.append(data)
-print("ndataset shape: ",np.array(ndataset).shape)
-trainX=np.reshape(np.array(ndataset),(batch_size,look_back,1))
-months=[31,28,31,30,31,30,31,31,30,31,30,31]
-for i in range(sum(months[:4])):
-    trainPredict = model.predict(trainX)
-    dataset[0][-1]=trainPredict[0][0]
-    print(scaler.inverse_transform(dataset)[0][-1],",")
-    ndataset[0]=ndataset[0][1:]
-    ndataset[0].append(trainPredict[0][0])
-    trainX=np.reshape(np.array(ndataset),(batch_size,look_back,1))
+number_of_predictions=len(dataframe1)-look_back-1
+entries=dataset[-len(dataframe1):]
+buffer=batch_size-(number_of_predictions)%batch_size
+print("buffer length",buffer)
+print("ghusne waale ki shape without buffer",entries.shape)
+if buffer>0:
+    ex=dataset[-buffer:]
+    entries=np.append(ex,entries)
+print("ghusne waale ki shape",entries.shape)
+trainX, trainY = create_dataset(entries, look_back)
+trainX = np.reshape(trainX, (trainX.shape[0], trainX.shape[1], 1))
+print(trainX.shape)
+######### RUN THE MODEL ########################
+trainPredict = model.predict(trainX)
+trainScore = math.sqrt(mean_squared_error(trainY, trainPredict[:,0]))
+print('Train Score: %.2f RMSE' % (trainScore))
+dataset[-len(trainPredict):]=trainPredict.reshape(dataset.shape)
+print(scaler.inverse_transform(dataset)[-len(trainPredict):])
+
