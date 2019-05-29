cb2dd76846b0ace477abfbd2cafb3d39670021ad
Anurag <tellanurag95@gmail.com>
2019-05-29 21:46:14 +0530

    new trends

--- LSTM/inference/latest_trend.py
+++ LSTM/inference/latest_trend.py
@@ -26,25 +26,20 @@ dataset = scaler.fit_transform(dataset)
 
 ######### LOAD THE MODEL #######################
 
-path=input("what is type of model you wish to load?")
+path="stacked_lstm" #input("what is type of model you wish to load?")
 model=load_model("../weights/"+path+".h5")
-print(model.summary())
+#print(model.summary())
 
 ######### MODEL DATA FEEDING ###################
 test = dataset[-len(dataframe1):,:]
-batch_size=int(input("Enter batch size"))
+batch_size=1 #int(input("Enter batch size"))
 look_back=3
-number_of_predictions=len(dataframe1)-look_back
-print(f"batch size: {batch_size} number of predictions: {number_of_predictions}")
-buffer=batch_size-(number_of_predictions)%batch_size
-print(f"buffer: {buffer}")
-if buffer>0:
-    ex=dataset[-buffer:]
-    test=np.append(ex,test)
 testX, testY = create_dataset(test, look_back)
 testX = np.reshape(testX, (testX.shape[0], testX.shape[1], 1))
 ######### RUN THE MODEL ########################
 testPredict = model.predict(testX, batch_size=batch_size)
 testPredict = scaler.inverse_transform(testPredict)
-print(testPredict[:number_of_predictions])
+print("test inflow predictions,")
+for item in testPredict:
+    print(item[0],",")
 
